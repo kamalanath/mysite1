e833ce339af840932310415ab5b319a028900447
Kamalanath Samarakoon <kamalanath@ieee.org>
2016-08-17 08:46:46 +0000

    Initial Add

---
+++ polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Choice, Question
+
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,						{'fields':  [ 'question_text']}),
+		('Date information', 		{'fields':  ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInLine]
+	list_display = ('question_text', 'pub_date' )
+	list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
+
+
